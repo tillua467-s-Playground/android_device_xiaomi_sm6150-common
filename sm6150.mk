59395aa51ec0f956e2f32a0b75ae12e1d1119446
tillua467 <tillua467@gmail.com>
2026-03-13 11:52:52 +0600

    Revert "sm8350-common: Build LunarisDolbyUI"
    This reverts commit 4c498660aedb7124cb2f6525474255e82bb72104.

--- sm6150.mk
+++ sm6150.mk
@@ -129,9 +129,6 @@ PRODUCT_COPY_FILES += \
 # Dolby
 $(call inherit-product, hardware/dolby/dolby.mk)
 
-PRODUCT_PACKAGES += \
-    LunarisDolby
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
